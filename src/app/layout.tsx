8df570395307464e89132561140ac8106f341fa2
John Paul Pacatang <59640442+JohnPaulPacatang@users.noreply.github.com>
2023-12-05 17:48:56 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
       <body>
         <Navbar />
         {children}
